03e8701163cc0dae58adc44a214fdac661b02add
CristianCeccon <cristian_ceccon45@hotmail.com>
2022-08-26 22:29:47 -0300

    Criação de DTO aluguel

---
+++ projetohexa/lib/domain/dto/aluguel_dto.dart
@@ -0,0 +1,13 @@
+class AluguelDTO {
+  int tempoMinimo;
+  int tempoAluguel;
+  int tempoMaximo;
+
+  double precoMinuto;
+
+  double valorPagar;
+
+  AluguelDTO(this.tempoMinimo, this.tempoAluguel, this.tempoMaximo,
+      this.precoMinuto, this.valorPagar);
+
+}
